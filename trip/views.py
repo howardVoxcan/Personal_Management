39171b6c07102936a7bffd91577cec9e81bfe87f
Võ Quang Nhật Hoàng <vqnhoang97tn@gmail.com>
2025-06-09 08:36:45 +0700

    Base settings, app, path, ... create

---
+++ trip/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpResponseForbidden
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+def trip(request):
+    pass
